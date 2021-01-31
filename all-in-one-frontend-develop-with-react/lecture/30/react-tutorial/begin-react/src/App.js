8eb83d60fff17b98c63b24caa14ca37f53819c7e
Siyun Lee <siyun1221@gmail.com>
2021-02-01 07:28:47 +0900

    Learn

--- all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/App.js
+++ all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/App.js
@@ -1,9 +1,34 @@
-import Counter from './Counter';
+import React, { useRef } from 'react';
 import InputSample from './InputSample';
 import UserList from './UserList';
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    console.log(nextId.current); //4
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
