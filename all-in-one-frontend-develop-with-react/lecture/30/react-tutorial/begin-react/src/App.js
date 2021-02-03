63f96a675f638674a4441559fbd946c80de4ce2e
Siyun Lee <siyun1221@gmail.com>
2021-02-04 07:26:02 +0900

    210204

--- all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/App.js
+++ all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/App.js
@@ -22,16 +22,19 @@ function App() {
       id: 1,
       username: 'velopert',
       email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
       username: 'tester',
       email: 'tester@example.com',
+      active: false,
     },
     {
       id: 3,
       username: 'liz',
       email: 'liz@example.com',
+      active: false,
     },
   ]);
 
@@ -53,6 +56,18 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <>
       <CreateUser
@@ -61,7 +76,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />;
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
--- all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/UserList.js
+++ all-in-one-frontend-develop-with-react/lecture/30/react-tutorial/begin-react/src/UserList.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{user.username}</b>
-      <span>({user.email})</span>
+      <b
+        style={{
+          color: active ? 'green' : 'black',
+          cursor: 'pointer',
+        }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
+      </b>
+      &nbsp;
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
